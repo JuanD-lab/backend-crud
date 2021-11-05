ed87e5822f89f98c82d81ebdc395e3d345725ab5
JuanD-lab <yo-juan@outlook.com>
2021-11-04 21:21:50 -0500

    created models & migrations

---
+++ src/migrations/20211105014813-create-usuarios-vinos-favoritos.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('usuarios_vinos_favoritos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      vino_id: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('usuarios_vinos_favoritos');
+  }
+};
---
+++ src/models/usuariosvinosfavoritos.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class UsuariosVinosFavoritos extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    UsuariosVinosFavoritos.init(
+        {
+            user_id: DataTypes.INTEGER,
+            vino_id: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: "UsuariosVinosFavoritos",
+            tableName: "usuarios_vinos_favoritos",
+            underscored: true,
+        }
+    );
+    return UsuariosVinosFavoritos;
+};
