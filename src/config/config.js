4a7e5583a52a163da84f8eac52f29cf1784cfd30
JuanD-lab <yo-juan@outlook.com>
2021-11-06 22:25:13 -0500

    test for vinos endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -13,6 +13,7 @@ module.exports = {
         username: process.env.DB_USERNAME_TEST,
         password: process.env.DB_PASSWORD_TEST,
         database: process.env.DB_NAME_TEST,
+        port: process.env.DB_PORT_TEST,
         host: process.env.DB_HOST_TEST,
         dialect: "postgres",
     },
