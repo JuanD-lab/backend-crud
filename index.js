b897176e643660ee46bc4724ba6ade8131f8cf5d
JuanD-lab <yo-juan@outlook.com>
2021-11-08 23:05:35 -0500

    create error handler for api

--- index.js
+++ index.js
@@ -22,4 +22,20 @@ app.get("/", (req, res) => res.json({ Crud: "1.0.1" }));
 app.use("/api", vinosRoutes);
 app.use("/api", usuariosRoutes);
 
+app.use((err, req, res, next) => {
+    if (err.name === "SequelizeValidationError") {
+        const errObj = { message: "Validation error", errors: [] };
+        err.errors.map((er) => {
+            errObj.errors.push({ [er.path]: er.message });
+        });
+        return res.status(403).send(errObj);
+    }
+    if (err.name === "SequelizeUniqueConstraintError") {
+        return res.status(409).send({ message: "Register already exists" });
+    }
+    return res
+        .status(500)
+        .send("Ups tenemos un problema en el servidor, intentalo más tarde!");
+});
+
 module.exports = app;
